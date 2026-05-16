3bdd3ddcc157e2ade8b3e9e123dcd3a128b73273
Mark Cyster <mcyster@gmail.com>
2026-05-16 09:10:39 -0700

    try aider

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Dev shell with aider-chat and Python";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = [
+          pkgs.aider-chat
+          pkgs.python3
+        ];
+      };
+    };
+}
